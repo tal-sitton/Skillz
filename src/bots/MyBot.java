b3978e22f0d42569500dde79ab108f0436944883
talsi <tal.sitton04@gmail.com>
2020-02-11 11:33:21 +0200

    it dosent work but....

--- src/bots/MyBot.java
+++ src/bots/MyBot.java
@@ -61,8 +61,37 @@ public class MyBot implements SkillzBot {
                             destination = getEnemyIceDesWithNewYeledKaka(game, myIceberg);
                             myPenguinAmountToSend = destination.penguinAmount + howManyEnemyPSent(game, destination) + destination.penguinsPerTurn
                                     * myIceberg.getTurnsTillArrival(destination) + 1;
+
                         }
                     }
+                    /*else {*/
+                    System.out.println("defence 1 " + howManyEnemyPSent(game, myIceberg) + " iceberg: " + myIceberg);
+                    int amountToSend = 0;
+                    if ((howManyEnemyPSent(game, myIceberg) > 0)) { /* defence! */
+                        System.out.println("defence 2");
+                        PenguinGroup locatedGroup = null;
+                        for (PenguinGroup penguinGroup : game.getEnemyPenguinGroups()) {
+
+                            if (penguinGroup.destination == myIceberg) {
+                                amountToSend = penguinGroup.penguinAmount - (myIceberg.penguinAmount + penguinGroup.turnsTillArrival * myIceberg.penguinsPerTurn + 1);
+                                System.out.println(amountToSend);
+                                locatedGroup = penguinGroup;
+                                break;
+                            }
+                        }
+                        for (Iceberg myI1 : game.getMyIcebergs()) {
+                            if (locatedGroup != null) {
+                                if (myI1.getTurnsTillArrival(myIceberg) <= locatedGroup.turnsTillArrival && myI1 != myIceberg) {
+                                    System.out.println("defence 4 -- end ( " + myI1 + " ):");
+                                    int myI1Amount = myI1.penguinAmount;
+                                    myI1.sendPenguins(myIceberg, amountToSend);
+                                    if (myI1Amount == myI1.penguinAmount + 2)
+                                        break;
+                                }
+                            }
+                        }
+                    }
+
                 }
             }
             int z = 0;
@@ -95,10 +124,6 @@ public class MyBot implements SkillzBot {
                 if (mTurns < penguinGroup.turnsTillArrival)
                     mTurns = penguinGroup.turnsTillArrival;
 
-                /*if (canHandle - howManyEnemyPSent(game,iceberg) > 0){                   //penguinGroup.penguinAmount > 0) {
-                    allICanHandleWith -= (canHandle - penguinGroup.penguinAmount);
-                }
-            */
             }
         }
         return iceberg.penguinAmount + gainEveryTurn * mTurns - howManyEnemyPSent(game, iceberg) + 1;
@@ -175,4 +200,6 @@ public class MyBot implements SkillzBot {
             }
         return first;
     }
+
+
 }
